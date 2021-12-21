ecb9f5464665a25cf15bc9e36a5b2d44de6a0cdf
Samuel Krupík <samo.krupik@gmail.com>
2021-12-21 22:33:31 +0100

    Added navbar to login and register

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -1,49 +1,54 @@
 <template>
-  <auth-card>
-    <div class="">
-      <auth-label for="email">Email</auth-label>
-      <auth-input type="email" id="email" v-model="register.email" />
-    </div>
-    <div class="mt-4">
-      <auth-label for="name">Meno</auth-label>
-      <auth-input type="text" id="name" v-model="register.name" />
-    </div>
-    <div class="mt-4">
-      <auth-label for="password">Heslo</auth-label>
-      <auth-input type="password" id="password" v-model="register.password" />
-    </div>
-    <div class="mt-4">
-      <auth-label for="password_confirmation">Kontrola hesla</auth-label>
-      <auth-input
-        type="password"
-        id="password_confirmation"
-        v-model="register.password_confirmation"
-      />
-    </div>
-    <div class="mt-8 flex flex-col items-center sm:flex-row-reverse">
-      <button
-        class="button-primary w-full rounded-lg sm:w-max"
-        @click="handleRegister"
-      >
-        Registrácia
-      </button>
-      <router-link to="/login" class="link mt-3 sm:mr-4 sm:mt-0"
-        >Už zaregistrovný?</router-link
-      >
-    </div>
-  </auth-card>
+  <div id="register">
+    <c-navigation />
+    <auth-card>
+      <div class="">
+        <auth-label for="email">Email</auth-label>
+        <auth-input type="email" id="email" v-model="register.email" />
+      </div>
+      <div class="mt-4">
+        <auth-label for="name">Meno</auth-label>
+        <auth-input type="text" id="name" v-model="register.name" />
+      </div>
+      <div class="mt-4">
+        <auth-label for="password">Heslo</auth-label>
+        <auth-input type="password" id="password" v-model="register.password" />
+      </div>
+      <div class="mt-4">
+        <auth-label for="password_confirmation">Kontrola hesla</auth-label>
+        <auth-input
+          type="password"
+          id="password_confirmation"
+          v-model="register.password_confirmation"
+        />
+      </div>
+      <div class="mt-8 flex flex-col items-center sm:flex-row-reverse">
+        <button
+          class="button-primary w-full rounded-lg sm:w-max"
+          @click="handleRegister"
+        >
+          Registrácia
+        </button>
+        <router-link to="/login" class="link mt-3 sm:mr-4 sm:mt-0"
+          >Už zaregistrovný?</router-link
+        >
+      </div>
+    </auth-card>
+  </div>
 </template>
 
 <script>
 import AuthInput from "@/components/auth/Input.vue";
 import AuthLabel from "@/components/auth/Label.vue";
 import AuthCard from "@/components/auth/AuthCard.vue";
+import CNavigation from "@/components/CNavigation.vue";
 export default {
   title: "Registrácia",
   components: {
     AuthInput,
     AuthLabel,
     AuthCard,
+    CNavigation,
   },
   data() {
     return {
